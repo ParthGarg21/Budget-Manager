a1647c6098df2603493498040569ce8545564c78
Parth Garg <parthsul@gmail.com>
2023-12-31 18:23:53 +0530

    changed client url

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "https://budget-manager-2lwb.onrender.com",
+    origin: "https://my-budget-manager-app.netlify.app/",
   })
 );
 app.use(cookieParser());
