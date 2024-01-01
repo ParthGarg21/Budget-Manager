4270f2e1d039b7316fd05f0ce7d2d7edae17e863
Parth Garg <parthsul@gmail.com>
2024-01-01 14:30:58 +0530

    add client url to backend

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: "http://localhost:3000",
+    origin: "https://my-budget-manager-app.netlify.app",
   })
 );
 // app.use(cookieParser());
